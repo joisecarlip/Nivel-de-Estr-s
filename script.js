5043560d68b5c15cddfd65386cdabe8cbc55bfa1
joisecarlip <157906384+joisecarlip@users.noreply.github.com>
2024-01-27 13:02:34 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,17 +2,29 @@ function predecir() {
     var edad = document.getElementById("edad").value;
     var sexo = document.getElementById("sexo").value;
 
-    fetch('http://127.0.0.1:5000/predecir', {
+    // Realizar solicitud al servidor Flask
+    fetch('http://127.0.0.1:5001/predecir', {
         method: 'POST',
         headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Type': 'application/x-www-form-urlencoded',
         },
         body: 'edad=' + edad + '&sexo=' + sexo,
     })
     .then(response => response.json())
     .then(resultado => {
-        alert("Nivel de estrés: " + resultado.Nivel_de_estres);
-
-        document.getElementById("resultado-container").innerHTML = "Nivel de estrés: " + resultado.Nivel_de_estres;
+        // Manejar la respuesta del servidor
+        mostrarResultado(resultado.Nivel_de_estres);
+    })
+    .catch(error => {
+        console.error('Error al realizar la solicitud:', error);
     });
 }
+
+function mostrarResultado(nivelEstres) {
+    // Puedes manejar cómo mostrar el resultado en tu interfaz de usuario
+    alert("Nivel de estrés: " + nivelEstres);
+
+    // También puedes actualizar elementos en el HTML
+    document.getElementById("resultado-container").innerHTML = "Nivel de estrés: " + nivelEstres;
+}
+
